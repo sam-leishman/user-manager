5c5438c3fd71d9955668cae14f9edf26874e6d18
sam-leishman <SAMUEL.LEISHMAN8727@STU.MTEC.EDU>
2021-02-03 07:39:19 -0700

    Added lookup page, users page, and set up view engine

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 const express = require('express');
-const uuid = require('uuid'); //.v4 ?
+const uuid = require('uuid');
+const fs = require('fs');
 
 const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(express.urlencoded({extended:false}))
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', { users: [] })
+})
+
+app.get('/form', (req, res) => {
+    res.render('form')
+})
+
+app.get('/users', (req, res) => {
+    res.render('users')
+})
+
+app.get('/lookup', (req, res) => {
+    res.render('lookup')
+})
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    let user = {};
+    user.id = uuid.v4();
+    user.username = req.body.name;
+    user.email = req.body.email;
+    user.age = req.body.age;
+    // fs.writeFileSync('./users.json', JSON.stringify(user));
+    res.render('users')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
